21d46bb72e2e71415d24fcfc6c5698325c4f5e6d
Jess Ritter <jessicalynn.w.ritter@gmail.com>
2017-05-07 14:22:36 -0700

    tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,18 +1,24 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 
 class TestingButtonInBrowser(LiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.FireFox()
+        driver = webdriver.Chrome
+        self.browser = driver()
 
     def tearDown(self):
         self.browser.quit()
 
     def testButton(self):
-        self.browser.get('http://jessicalynnritter.pythonanywhere.com/post/1/#collapseExample')
-
+        driver = self.browser.get('http://jessicalynnritter.pythonanywhere.com/post/1/#collapseExample')
+        driver_wait = WebDriverWait(driver, 60)
+        driver_wait.until(EC.presence_of_element_located(self.browser.find_element_by_id('collapseButton')))
         button = self.browser.find_element_by_id('collapseButton')
         button.click()
         text = self.browser.find_element_by_id('collapseExample')
